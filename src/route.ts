cd740efb3b84977b6740238f41450a9075ccfe01
Jose Lucas <joselucas.dev@gmail.com>
2024-05-29 04:19:28 -0300

    feat:add route.ts

---
+++ src/route.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import { CreateUserController } from "./useCases/CreateUser/CreateUserController";
+
+import { AuthenticateUserController } from "./useCases/authenticateUser/AuthenticateUserController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { RefreshTokenUserController } from "./useCases/refreshTokenUser/refreshTokenUserController";
+
+const router = Router();
+
+const createUserController: CreateUserController = new CreateUserController();
+
+const authenticateUserController: AuthenticateUserController =
+  new AuthenticateUserController();
+
+const refreshTokenUserController = new RefreshTokenUserController();
+
+router.post("/users", createUserController.handle);
+
+router.post("/login", authenticateUserController.handle);
+
+router.post("/refresh_token", refreshTokenUserController.handle);
+
+router.get("/courses", ensureAuthenticated, (request, response) => {
+  return response.json([
+    {
+      id: 1,
+      name: "NODEJS",
+    },
+    {
+      id: 2,
+      name: "ANGULAR",
+    },
+    {
+      id: 3,
+      name: "JAVA",
+    },
+    {
+      id: 4,
+      name: "REACT",
+    },
+  ]);
+});
+
+export { router };
